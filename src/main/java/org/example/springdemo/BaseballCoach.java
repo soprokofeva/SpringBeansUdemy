b90fabe77326b93e6132773875d47da79c799903
soprokofeva <54109089+soprokofeva@users.noreply.github.com>
2020-09-26 22:23:47 +0300

    4SpringBeansIoC_XML

---
+++ src/main/java/org/example/springdemo/BaseballCoach.java
@@ -0,0 +1,9 @@
+package org.example.springdemo;
+
+public class BaseballCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes on batting practice";
+    }
+}
---
+++ src/main/java/org/example/springdemo/Coach.java
@@ -0,0 +1,5 @@
+package org.example.springdemo;
+
+public interface Coach {
+    public String getDailyWorkout();
+}

8d967b8d73e07349fa986c58f4d2901169de3cdd
soprokofeva <54109089+soprokofeva@users.noreply.github.com>
2020-10-04 19:48:05 +0300

    5SpringBeansDependencyInjection_ConstructorWithArg_XML

---
+++ src/main/java/org/example/springdemo/SetterDemoApp.java
@@ -0,0 +1,23 @@
+package org.example.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+    public static void main(String[] args) {
+
+//        load the spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+
+//        retrieve bean from spring container
+        CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+
+//        call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyFortune());
+
+
+//        close context
+        context.close();
+    }
+}
